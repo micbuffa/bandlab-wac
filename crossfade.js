d2dd9b533c26ac5f22a78ade2322f05abe2aed8f
jivers <john.ivers@gmail.com>
2017-08-23 01:26:51 -0700

    Add rhodes.adv, none setting for harmonizer, Crossfade comments

--- crossfade.js
+++ crossfade.js
@@ -1,5 +1,23 @@
 import { audioCtx } from './sampler.js';
 
+// -------------------------------------------------------------------------------------------------
+// Play position starts at zero, proceeds to loop end, then goes back to loop start.
+// During the crossfade, starting crossfade samples before loopEnd, we fade out at the play
+// position, and fade in before loopStart.
+//
+//
+//          loopStart   crossfadeStart
+//             |          |
+//             |__________|
+//             /          \ loopEnd
+//            /            \ |
+//           /              \|
+//  ________/________________\______
+//                        |  |
+//            crossfade ->|  |<-
+//
+// -------------------------------------------------------------------------------------------------
+
 const SAMPLE_RATE = audioCtx.sampleRate;
 
 export function includeCrossfade(audioBuffer, { loopStart, loopEnd, crossfade }) {
@@ -11,16 +29,4 @@ function createCrossfade(arrayBuffer, { loopStart, loopEnd, crossfade }) {
 
 function secondsToSamples(seconds) {
   return Math.round(seconds * SAMPLE_RATE);
-}
-
-function fadeIn(arrayBuffer, start, end) {
-  for (let i = start; i < end; i++) {
-    arrayBuffer[i] = arrayBuffer[i] * Math.sin(((i - start) * (Math.PI / ((end - start) * 2))));
-  }
-}
-
-function fadeOut(arrayBuffer, start, end) {
-  for (let i = start; i < end; i++) {
-    arrayBuffer[i] = arrayBuffer[i] * Math.cos(((i - start) * (Math.PI / ((end - start) * 2))));
-  }
 }
--- working-version/crossfade.js
+++ working-version/crossfade.js
@@ -1,5 +1,23 @@
 import { audioCtx } from './sampler.js';
 
+// -------------------------------------------------------------------------------------------------
+// Play position starts at zero, proceeds to loop end, then goes back to loop start.
+// During the crossfade, starting crossfade samples before loopEnd, we fade out at the play
+// position, and fade in before loopStart.
+//
+//
+//          loopStart   crossfadeStart
+//             |          |
+//             |__________|
+//             /          \ loopEnd
+//            /            \ |
+//           /              \|
+//  ________/________________\______
+//                        |  |
+//            crossfade ->|  |<-
+//
+// -------------------------------------------------------------------------------------------------
+
 const SAMPLE_RATE = audioCtx.sampleRate;
 
 export function includeCrossfade(audioBuffer, { loopStart, loopEnd, crossfade }) {
