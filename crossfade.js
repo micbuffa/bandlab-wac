1311768d7f37686356aa839bdb8ebce73fb2f989
Gilles Piou <gilles.piou@gmail.com>
2017-08-23 10:19:36 +0100

    Add back fade in/out functions

--- crossfade.js
+++ crossfade.js
@@ -30,3 +30,15 @@ function createCrossfade(arrayBuffer, { loopStart, loopEnd, crossfade }) {
 function secondsToSamples(seconds) {
   return Math.round(seconds * SAMPLE_RATE);
 }
+
+function fadeIn(arrayBuffer, start, end) {
+  for (let i = start; i < end; i++) {
+    arrayBuffer[i] = arrayBuffer[i] * Math.sin(((i - start) * (Math.PI / ((end - start) * 2))));
+  }
+}
+
+function fadeOut(arrayBuffer, start, end) {
+  for (let i = start; i < end; i++) {
+    arrayBuffer[i] = arrayBuffer[i] * Math.cos(((i - start) * (Math.PI / ((end - start) * 2))));
+  }
+}
